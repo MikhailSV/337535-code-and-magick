064727e0895c57137c53b2506ccf4d90ac9d1b15
MikhailSV <MihalichSV@gmail.com>
2017-01-30 21:46:17 +0300

    Исправлены комментарии по функциям

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,43 @@
 'use strict';
 
+var creatureRandomColor = function (names) {
+  for (var i = 0; i < names.length; i++) {
+    var colorStationIfirst = 'rgba(0, 0,';
+    var colorStationIsecond = ')';
+    var colorGamerStatShade = (Math.floor(Math.random() * 10)) * 0.1;
+    var colorGamerStatСlarity = 215 + (Math.floor(Math.random() * 50));
+    var colorStationI = colorStationIfirst + colorGamerStatСlarity + ',' + colorGamerStatShade + colorStationIsecond;
+  }
+  return colorStationI;
+};
+
+var drawStatistic = function (ctx, names, times) {
+  var statHeight = 150;
+  var statWidth = 40;
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    var name = names[i];
+    var cutTime = Math.round(time / 100);
+
+    if (name === 'Вы') {
+      var colorForStatUser = 'rgba(255, 0, 0, 1)';
+    } else {
+      var colorForStatUser = creatureRandomColor(names);
+    }
+
+    ctx.fillStyle = colorForStatUser;
+    ctx.fillRect(150 + 90 * i, (statHeight - cutTime), statWidth, cutTime + 80);
+
+    ctx.fillStyle = '#000';
+    ctx.font = '14px PT Mono';
+    ctx.fillText(name, 150 + 90 * i, 250);
+
+    ctx.fillStyle = '#000';
+    ctx.font = '14px PT Mono';
+    ctx.fillText(cutTime, 150 + 90 * i, (statHeight - cutTime) - 10);
+  }
+};
+
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(100, 10, 420, 270);
@@ -13,32 +51,5 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 110, 45);
   ctx.fillText('Список результатов', 110, 60);
 
-  var statHeight = 150;
-  var statWidth = 40;
-  var colorStationIfirs = 'rgba(0, 0,';
-  var colorStationIsecond = ')';
-  for (var i = 0; i < names.length; i++) {
-    var time = times[i];
-    var name = names[i];
-    var colorGamerStatA = (Math.floor(Math.random() * 10)) * 0.1;
-    var colorGamerStatB = 255 - (Math.floor(Math.random() * 50));
-    if (name === 'Вы') {
-      var colorStationI = 'rgba(255, 0, 0, 1)';
-    } else {
-      var colorStationI = colorStationIfirs + colorGamerStatB + ',' + colorGamerStatA + colorStationIsecond;
-
-    }
-    var catTime = Math.round(time / 100)
-
-    ctx.fillStyle = colorStationI;
-    ctx.fillRect(150 + 90 * i, (statHeight - catTime), statWidth, catTime + 80);
-
-    ctx.fillStyle = '#000';
-    ctx.font = '14px PT Mono';
-    ctx.fillText(name, 150 + 90 * i, 250);
-
-    ctx.fillStyle = '#000';
-    ctx.font = '14px PT Mono';
-    ctx.fillText(catTime, 150 + 90 * i, (statHeight - catTime) - 10);
-  }
+  drawStatistic(ctx, names, times);
 };
